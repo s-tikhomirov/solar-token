03c952975c7f84422b2cfbbc9736e3653872ff39
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-08-16 21:31:40 +0200

    [WIP] clean code - now an ERC223 token

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -8,23 +8,15 @@ pragma solidity ^0.4.13;
 import "./ERC223/ERC223BasicToken.sol";
 
 contract MetaCoin is ERC223BasicToken {
-	mapping (address => uint) balances;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-	function MetaCoin() {
-		balances[tx.origin] = 10000;
-	}
+	function MetaCoin() { }
 
 	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		Transfer(msg.sender, receiver, amount);
+		transfer(receiver, amount);
 		return true;
 	}
 
 	function getBalance(address addr) returns(uint) {
-		return balances[addr];
+		return balanceOf(addr);
 	}
 }
