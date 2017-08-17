10fe8204e8272626b2d0442eac07242e87d8a1c4
Anders Kalør <anders@kaloer.com>
2017-08-17 20:48:53 +0200

    Updated frontend

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,22 +6,22 @@ import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+import metacoin_artifacts from '../../build/contracts/Voting.json'
 
 var ROLE_BUILDING_OWNER = 0;
 var ROLE_COMPANY = 1;
 var ROLE_RESIDENT = 2;
 var ids = {
-    "0x0120fe6e3aa2c936e6d2f88062b266c46c408f98": {"role": ROLE_BUILDING_OWNER, "name": "Name 1"},
-    "0xa8fc189694dc8ba03c027d470b77c09b7d13471e": {"role": ROLE_COMPANY, "name": "Name 2"},
-    "0xdbf73038f3e4a5d27e38004c36dc6ce82165794f": {"role": ROLE_RESIDENT, "name": "Name 3"},
-    "0x692b50dc6d0021a73ec50fcd3ad78d3c121be360": {"role": ROLE_RESIDENT, "name": "Name 4"},
-    "0xc396dfc357c849c5e4fbbe1f130eb469675af895": {"role": ROLE_RESIDENT, "name": "Name 5"},
-    "0x07efc80760137f58a43e44a9bede15da8053d730": {"role": ROLE_RESIDENT, "name": "Name 6"},
-    "0xfe8699b505180f51966e56f118e66544dea311bd": {"role": ROLE_RESIDENT, "name": "Name 7"},
-    "0x276ae02aa2710bebc571f3647d64d9b440f998d6": {"role": ROLE_RESIDENT, "name": "Name 8"},
-    "0x43882f2be5ccf7ba1cf848e202683385d5fb93bb": {"role": ROLE_RESIDENT, "name": "Name 9"},
-    "0x76c780eb591481c086e1a0574b16c4f52e5aa352": {"role": ROLE_RESIDENT, "name": "Name 10"},
+    "0x0120fe6e3aa2c936e6d2f88062b266c46c408f98": {"role": ROLE_BUILDING_OWNER, "name": "Name 1", "approved": false},
+    "0xa8fc189694dc8ba03c027d470b77c09b7d13471e": {"role": ROLE_COMPANY, "name": "Name 2", "approved": false},
+    "0xdbf73038f3e4a5d27e38004c36dc6ce82165794f": {"role": ROLE_RESIDENT, "name": "Name 3", "approved": false},
+    "0x692b50dc6d0021a73ec50fcd3ad78d3c121be360": {"role": ROLE_RESIDENT, "name": "Name 4", "approved": false},
+    "0xc396dfc357c849c5e4fbbe1f130eb469675af895": {"role": ROLE_RESIDENT, "name": "Name 5", "approved": false},
+    "0x07efc80760137f58a43e44a9bede15da8053d730": {"role": ROLE_RESIDENT, "name": "Name 6", "approved": false},
+    "0xfe8699b505180f51966e56f118e66544dea311bd": {"role": ROLE_RESIDENT, "name": "Name 7", "approved": false},
+    "0x276ae02aa2710bebc571f3647d64d9b440f998d6": {"role": ROLE_RESIDENT, "name": "Name 8", "approved": false},
+    "0x43882f2be5ccf7ba1cf848e202683385d5fb93bb": {"role": ROLE_RESIDENT, "name": "Name 9", "approved": false},
+    "0x76c780eb591481c086e1a0574b16c4f52e5aa352": {"role": ROLE_RESIDENT, "name": "Name 10", "approved": false},
 };
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
@@ -53,14 +53,44 @@ window.App = {
       }
 
       accounts = accs;
-      account = accounts[0];
+        account = accounts[0];
+
+        MetaCoin.deployed().then(function(instance) {
+            console.log(instance);
+            instance.state.call({from: account}).then(function(amount) {
+                console.log(amount);
+            });
+        });
 
       self.updateUI();
-      self.refreshBalance();
+      //self.refreshBalance();
     });
   },
 
-  updateUI: function() {
+    updateUI: function() {
+        // Update table
+        var residents_table = document.getElementById("residents_table");
+        console.log(residents_table);
+        for(var id in ids) {
+            console.log(id);
+        
+            if(ids[id].role == ROLE_RESIDENT) {
+                var tr = document.createElement("tr");
+                var td_name = document.createElement("td");
+                var td_shares = document.createElement("td");
+                var td_approved = document.createElement("td");
+                name = ids[id].name + " (" + id + ")";
+                td_name.appendChild(document.createTextNode(name));
+                td_shares.appendChild(document.createTextNode(ids[id].shares));
+                td_approved.appendChild(document.createTextNode(ids[id].approved));
+                tr.appendChild(td_name);
+                tr.appendChild(td_shares);
+                tr.appendChild(td_approved);
+                residents_table.appendChild(tr);
+                console.log(tr);
+            }
+        }
+        
       // Get account type
       if(ids[account] !== null) {
           var building_owner_area = document.getElementById("building_owner_area");
@@ -77,7 +107,21 @@ window.App = {
               residents_area.classList.remove("hidden");
           }
       }
-  },
+    },
+
+    refreshVoting: function() {
+        var self = this;
+        var meta;
+        MetaCoin.deployed().then(function(instance) {
+            meta = instance;
+            return meta.getVoting.call({from: account});
+        }).then(function(value) {
+            console.log(value);
+            self.updateUI();
+        }).catch(function(e) {
+            console.log(e);
+        });
+    },
 
     startCampaign: function() {
         var start_campaign_btn = document.getElementById("start_compaign_btn");
@@ -89,33 +133,17 @@ window.App = {
           return meta.startVoting({from: account})
       }).then(function() {
           start_campaign_btn.value = "Campaign started!";
-          //self.refreshBalance();
+          //self.updateUI();
       }).catch(function(e) {
           console.log(e);
-          start_campaign_btn.value = "Campaign started!";
+          start_campaign_btn.value = "Error! See log.";
           //self.setStatus("Error sending coin; see log.");
       });
   },
 
   setStatus: function(message) {
     var status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-
-  refreshBalance: function() {
-    var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
+    //status.innerHTML = message;
   },
 
   transfer: function() {
