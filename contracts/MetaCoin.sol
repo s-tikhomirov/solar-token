96f6b7c24bed14cea998376545c5b8c6b049ee83
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-08-17 12:11:36 +0200

    remove comment

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,10 +1,5 @@
 pragma solidity ^0.4.13;
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
-
 import "./ERC223/ERC223BasicToken.sol";
 
 contract MetaCoin is ERC223BasicToken {
